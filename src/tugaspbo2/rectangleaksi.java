a80c2ca5f99bcebece899c2eee687f9ecd8ee7f6
yafinoor <mnyafi777@gmail.com>
2018-04-03 09:39:42 +0800

    mengisi nilai attribute

--- src/tugaspbo2/rectangleaksi.java
+++ src/tugaspbo2/rectangleaksi.java
@@ -3,6 +3,8 @@ package tugaspbo2;
 public class rectangleaksi {
     public static void main(String[] args) {
         rectangle rl = new rectangle ();
+        rl.panjang = 9;
+        rl.lebar = 4;
     }
    
 }
