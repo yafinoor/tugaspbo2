cc1f3f8287205f5bd5b6b80be3f49581d52e7b9c
yafinoor <mnyafi777@gmail.com>
2018-04-03 09:31:16 +0800

    membuat attribute dan behavior

--- src/tugaspbo2/rectangle.java
+++ src/tugaspbo2/rectangle.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugaspbo2;
-
-/**
- *
- * @author Muhammad Noor Yafi
- */
 public class rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakinfo() {
+        System.out.println("======================");
+        System.out.println("panjang : "+panjang);
+        System.out.println("lebar   : "+lebar);
+        System.out.println("======================");
+    }
 }
